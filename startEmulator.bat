b6caa5089cffad047acdf3ffc34c2d797eb3ff51
Zaid Pathan <zaidpathan339@gmail.com>
2019-08-15 10:20:01 +0530

    The flow of the code has been changed.
    Code to remove usage of cd command and istead of changing the current directory to specific location. Now the commands are directly executed with specific directory path.

--- startEmulator.bat
+++ startEmulator.bat
@@ -1,8 +1,23 @@
 @echo off
 rem "Code to turn off echo to avoid printing commands on console"
 
-rem "code to change current directory to a specific directory where android sdk tools installed"
-rem "OLD CODE : cd C:\Android\sdk\tools"
+rem "Code to clear the screen"
+cls
+
+rem "NEW CODE : Code to read the second line from the reactConfiguration.settings file"
+setlocal enableextensions enabledelayedexpansion
+set firstLine=2
+for /f "delims=" %%i in (reactConfiguration.settings) do (
+
+    rem "Code to run the command without changing the directory"
+
+    if !firstLine!==1 %%i\emulator -list-avds
+    set firstLine=1
+)
+endlocal
+
+rem "code to ask to the user about the device name want to start with emulator"
+set /P DeviceName="Enter Device Name you want to start in emulator : "
 
 rem "Code to clear the screen"
 cls
@@ -11,34 +26,10 @@ rem "NEW CODE : Code to read the second line from the reactConfiguration.setting
 setlocal enableextensions enabledelayedexpansion
 set firstLine=2
 for /f "delims=" %%i in (reactConfiguration.settings) do (
-    if !firstLine!==1 cd %%i
+
+    rem "code to start the specific emulator by executing the command without changing the current directory"
+    
+    if !firstLine!==1 %%i\emulator -avd %DeviceName%
     set firstLine=1
-)
-endlocal
-
-rem "Code to clear the screen"
-cls
-
-rem "code to display available or created emulator devices list"
-emulator -list-avds
-
-rem "code to ask to the user about the device name want to start with emulator"
-set /P DeviceName="Enter Device Name you want to start in emulator : "
-
-rem "Code to clear the screen"
-cls
-
-rem "code to start the specific emulator"
-emulator -avd %DeviceName%
-
-rem "code to change specific directory to previous directory from where execution starts"
-rem "OLD CODE : cd C:\Users\COOL ZAID\Documents\React_Native_Projects"
-
-rem "NEW CODE : Code to read the first line from the reactConfiguration.settings file"
-setlocal enableextensions enabledelayedexpansion
-set firstLine=1
-for /f "delims=" %%i in (reactConfiguration.settings) do (
-    if !firstLine!==1 cd %%i
-    set firstLine=0
 )
 endlocal
